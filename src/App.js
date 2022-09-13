e0c88dbdb172a08ed902b8e045046f2473d93fe6
unknown <rakibulanas033@gmail.com>
2022-09-14 00:23:00 +0600

    add plus minus button

--- src/App.js
+++ src/App.js
@@ -25,13 +25,55 @@ function App() {
 		//const newProduct = [...selected, product];
 		getselected(newCart);
 	};
+	const inrcreament = (product) => {
+		let newCart = [];
+		const exists = selected.find(
+			(productSingle) => productSingle.id === product.id
+		);
+		if (!exists) {
+			product.quantity = 1;
+			newCart = [...selected, product];
+		} else {
+			const rest = selected.filter(
+				(productSingle) => productSingle.id !== product.id
+			);
+			exists.quantity = exists.quantity + 1;
+			newCart = [...rest, exists];
+		}
+		//const newProduct = [...selected, product];
+		getselected(newCart);
+	};
+	const deccreament = (product) => {
+		console.log(product);
+		let newCart = [];
+		const exists = selected.find(
+			(productSingle) => productSingle.id === product.id
+		);
+		if (!exists) {
+			product.quantity = 1;
+			newCart = [...selected, product];
+		} else {
+			const rest = selected.filter(
+				(productSingle) => productSingle.id !== product.id
+			);
+			exists.quantity = exists.quantity - 1;
+			newCart = [...rest, exists];
+		}
+		//const newProduct = [...selected, product];
+		getselected(newCart);
+	};
 	//console.log(selected.map((v) => v.price * v.quantity));
 	return (
 		<div className="App">
 			<Navbar cart={selected} />
 			<Routes>
 				<Route path="/" element={<Home button={handleProducts} />} />
-				<Route path="/cart" element={<Cart cart={selected} />} />
+				<Route
+					path="/cart"
+					element={
+						<Cart cart={selected} plus={inrcreament} minus={deccreament} />
+					}
+				/>
 			</Routes>
 		</div>
 	);
--- src/Component/Cart/Cart.js
+++ src/Component/Cart/Cart.js
@@ -1,14 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-const Cart = ({ cart }) => {
-	let total = 0;
-	for (const product of cart) {
-		total = total + product.price * product.quantity;
-	}
+const Cart = ({ cart, plus, minus }) => {
 	return (
 		<div className="cart bg-slate-200">
 			<div className="container mx-auto py-14">
 				<div className="cart bg-white p-10 mx-auto w-11/12 shadow-sm rounded-sm">
+					<Link to="/">
+						<button className="btn btn-primary mb-5">back</button>
+					</Link>
 					<div className="overflow-x-auto">
 						<table className="table w-full bg-white">
 							<thead>
@@ -29,12 +29,18 @@ const Cart = ({ cart }) => {
 										</td>
 										<td>
 											<div className="flex gap-4 content-center">
-												<button className="btn btn-sm text-xl font-bold text-white">
-													+
+												<button
+													className="btn btn-sm text-xl font-bold text-white"
+													onClick={() => minus(item)}
+												>
+													-
 												</button>
 												<div className="text-xl font-bold">{item.quantity}</div>
-												<button className="btn btn-sm text-xl font-bold text-white">
-													-
+												<button
+													className="btn btn-sm text-xl font-bold text-white"
+													onClick={() => plus(item)}
+												>
+													+
 												</button>
 											</div>
 										</td>
